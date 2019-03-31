6a1ac7899399819552f472db0336903a9f922548
tiagojordao <jordao.kbral@gmail.com>
2019-03-31 01:55:29 -0300

    STUB == Date

--- src/date.cpp
+++ src/date.cpp
@@ -1,2 +1,30 @@
 #include "date.h"
 
+	Date::date ():day(0),month(0),year(0){
+
+	}
+	Date::date (const int& d, const int& m, const int& y):day(d), month(m), year(y){
+
+	}
+	void Date::setDay (int day_){
+
+	}
+	void Date::setMonth (int month_){
+
+	}
+	void Date::setYear (int year_){
+
+	}
+
+	date Date::operator ++ (){
+
+	}
+	date Date::operator ++ (int){
+
+	}
+	date Date::operator -- (){
+
+	}
+	date Date::operator -- (int){
+
+	}
